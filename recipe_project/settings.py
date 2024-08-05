a91dea8a5f53966888baf78c9c48748df57cfd15
Yufan <yufanx8@gmail.com>
2024-08-05 13:45:45 -0700

    set debug to false for deployment

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
